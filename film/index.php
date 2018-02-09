a6345f1a3a28a511f5c293fa927199ab27be77d7
Teymur Hesenzade <teymur.hesenzade@hotmail.com>
2018-02-09 21:32:52 +0400

    Add files via upload

---
+++ film/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Şaşşaq</title>
+	<link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body class="bdy">
+	<div style="margin-top: 3%;">
+		<div>
+			<div class="bar container31">
+				<div class="left-bar">
+					Logo
+				</div>
+				<div class="right-bar">
+					<a href="#"><img src="res/insta.png"></a>
+					<a href="#"><img src="res/face.png"></a>
+					<a href="#"><img src="res/git.png"></a>
+				</div>
+			</div>
+			<div class="bar container31">
+				<div class="left-bar col">
+					<a href="#" class="bar-links"><b>GENRE</b></a>
+					<a href="#" class="bar-links"><b>MOVIES</b></a>
+					<a href="#" class="bar-links"><b>TV SHOWS</b></a>
+					<a href="#" class="bar-links"><b>ANIMES</b></a>
+				</div>
+				<div class="right-bar col">
+					<input type="text" name="search" class="src-inp" placeholder="Search..">
+					<img src="res/account2x.png" style="">
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
